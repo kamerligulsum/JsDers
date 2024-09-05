5a08219a8c70cf7e70969ca0e4d8bd8836a1e509
kamerligulsum <103079330+kamerligulsum@users.noreply.github.com>
2024-09-05 13:48:13 +0300

    Add files via upload

---
+++ degiskenler/script.js
@@ -0,0 +1,27 @@
+// Değişkenler
+
+var age;
+console.log(age) // undefined
+
+age = 20;
+console.log(age) // 20
+
+var fullname = 'Gülsüm Kamer';
+console.log(fullname) // Gülsüm Kamer
+
+/*
+ Değişken Tanımlama Kuralları
+ ** Değişken isimleri rakam ile başlayamaz.
+    var 1yas; => hatalı
+    var yas1; => geçerli
+    var _yas; => geçerli
+    
+ ** Komut isimleriyle tanımlama yapılamaz. Örneğin if kelimesi dğeişken ismi olamaz.
+ ** Büyük küçük harf duyarlılığı vardır.
+
+    var firstName = 'Gülsüm';
+    var FirstName = 'Kamer';
+    
+    Farklı değişken isimleridir.
+ ** Değişken isimlerinde türkçe karakter kullanmamalıyız.   
+*/
